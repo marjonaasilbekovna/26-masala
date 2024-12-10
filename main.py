1e7205fc5b81c811474ac9d69f0980e5158fe914
marjonaasilbekovna <abdurahmonovamarjona@gmail.com>
2024-12-10 15:14:08 +0300

    21-masala

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 n,k=map(int,input().split())
-print(k%n)
+print(k%n)a, b, c = map(int, input().split())
+A = (a + 1) // 2 
+B = (b + 1) // 2  
+C = (c + 1) // 2  
+
+umumiy = A + B + C
+
+print(umumiy)
