7d7dfa6471980cd8b38ff2e5bfae4d8cdc8c16a2
marjonaasilbekovna <abdurahmonovamarjona@gmail.com>
2024-12-10 19:11:57 +0300

    25-masala

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-h1, m1, s1 = map(int, input().split())
-h2, m2, s2 = map(int, input().split())
-bir = h1 * 3600 + m1 * 60 + s1
-ikki = h2 * 3600 + m2 * 60 + s2
-
-vaqt = ikki - bir
-
-print(vaqt)
+N = int(input())
+hours = (N // 3600) % 24
+minutes = (N % 3600) // 60
+seconds = N % 60
+result = f"{hours}:{minutes:02}:{seconds:02}"
+print(result)
