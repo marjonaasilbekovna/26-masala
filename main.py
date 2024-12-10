374f43a0a357a8c664058e518a79445ca47a18d7
marjonaasilbekovna <abdurahmonovamarjona@gmail.com>
2024-12-10 15:28:57 +0300

    24-masala

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-n=int(input())
-m=n//10
-print(m)
+h1, m1, s1 = map(int, input().split())
+h2, m2, s2 = map(int, input().split())
+bir = h1 * 3600 + m1 * 60 + s1
+ikki = h2 * 3600 + m2 * 60 + s2
+
+vaqt = ikki - bir
+
+print(vaqt)
